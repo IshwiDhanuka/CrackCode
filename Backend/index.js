379f37505185376b63e9f5a15fb226574f95c001
IshwiDhanuka <147974816+IshwiDhanuka@users.noreply.github.com>
2025-09-18 00:13:58 +0530

    Update proxy-run

--- Backend/index.js
+++ Backend/index.js
@@ -45,9 +45,9 @@ app.use('/api/submissions', submissionsRoutes);
 // Compiler Proxy Route
 app.post('/proxy-run', async (req, res) => {
   try {
-    const problem = await Problems.findOne({slug:req.body.slug})
-    const testcases = await Testcase.find({problemId:problem.id})
-    const response = await axios.post(`${process.env.COMPILER_URL}/run`, {...req.body, ...problem, testcases});
+    const problem = await Problems.findOne({slug:req.body.slug});
+    const testcases = (await Testcase.find({problemId:problem.id})).toJSON();
+    const response = await axios.post(`${process.env.COMPILER_URL}/run`, {...req.body, ...problem.toJSON(), testcases});
     res.json(response.data);
   } catch (err) {
     console.error("Compiler proxy error:", err);
